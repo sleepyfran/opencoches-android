1270eba90501fd0db1b9836fe089ded9aaf385b1
Fran González <fgonzaleva@gmail.com>
2016-10-01 23:15:39 +0200

    Preparando el proyecto para lo que se viene

--- ForoCarrosAndroid/app/src/main/java/io/spaceisstrange/opencoches/ui/activities/ThreadActivity.kt
+++ ForoCarrosAndroid/app/src/main/java/io/spaceisstrange/opencoches/ui/activities/ThreadActivity.kt
@@ -1,7 +1,10 @@
 package io.spaceisstrange.opencoches.ui.activities
 
+import android.animation.Animator
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.animation.AccelerateInterpolator
 import io.spaceisstrange.opencoches.R
 import io.spaceisstrange.opencoches.adapters.PostAdapter
 import io.spaceisstrange.opencoches.api.model.Post
@@ -31,57 +34,6 @@ import kotlinx.android.synthetic.main.activity_thread.*
  */
 
 class ThreadActivity : BaseActivity() {
-    /**
-     * Adapter del post encargado de darnos esas preciosas vistas en forma de post
-     */
-    val postAdapter = PostAdapter()
-
-    /**
-     * Layout manager de la recycler view
-     */
-    val layoutManager = PreCacheLayoutManager(this)
-
-    /**
-     * Página actual del hilo
-     */
-    var threadActualPage = 1
-
-    /**
-     * Variables para hacer el RecyclerView infinito
-     */
-    var previousTotalItemCount = 0
-    var visibleItemThreshold = 15
-    var firstVisibleItemPosition = 0
-    var visibleItemCount = 0
-    var totalItemCount = 0
-    var loadingContent = true
-
-    /**
-     * Carga el hilo del link especificado y añade el contenido al adapter
-     */
-    fun loadThread(link: String, page: Int?, onLoad: (posts: MutableList<Post>) -> Unit) {
-        FCThreadObservable.create(link, page).subscribe(
-                {
-                    posts ->
-
-                    // Ocultamos la carita del error si no lo está ya
-                    hideErrorMessage(vError)
-
-                    // ¡Hemos cargado el hilo!
-                    srlPostList.isRefreshing = false
-                    onLoad(posts)
-                },
-                {
-                    error ->
-
-                    srlPostList.isRefreshing = false
-
-                    // Mostramos la carita cuca con el error
-                    postAdapter.updatePosts(mutableListOf())
-                    showErrorMessage(vError)
-                }
-        )
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -98,75 +50,18 @@ class ThreadActivity : BaseActivity() {
         // Mostramos el título del hilo
         showCustomTitleOnToolbar(threadTitle)
 
-        // Configuramos la RecyclerView
-        rvPostList.adapter = postAdapter
-        rvPostList.layoutManager = layoutManager
+        // Cargamos el número total de páginas del hilo
+        FCThreadPagesObservable.create(threadLink).subscribe(
+                {
+                    pages ->
 
-        // Mostramos el hilo como cargando
-        srlPostList.isRefreshing = true
 
-        // Configuramos la opción del onClick sobre la foto del usuario
-        postAdapter.onUserClick = {
-            post ->
+                },
+                {
+                    error ->
 
-            // Mostramos el Bottom Sheet con las opciones
-            UserProfileDialog.newInstance(post.posterUsername, post.posterId, post.posterPictureLink)
-                    .show(supportFragmentManager, null)
-        }
-
-        // Obtenemos los posts del hilo y populamos la RecyclerView con ellos
-        loadThread(threadLink, null, { posts -> postAdapter.updatePosts(posts) })
-
-        // Cargamos también el hilo cuando el usuario haga un swipe to refresh
-        srlPostList.setOnRefreshListener {
-            loadThread(threadLink, null, { posts -> postAdapter.updatePosts(posts) })
-        }
-
-        // Configuramos la RecyclerView para ser "infinita"
-        // Basado en: http://stackoverflow.com/a/26561717
-        rvPostList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-
-                firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
-                totalItemCount = layoutManager.itemCount
-                visibleItemCount = recyclerView.childCount
-
-                if (loadingContent) {
-                    if (totalItemCount > previousTotalItemCount) {
-                        loadingContent = false
-                        previousTotalItemCount = totalItemCount
-                    }
+                    // Silenciamos los errores :)
                 }
-
-                if (!loadingContent && (totalItemCount - visibleItemCount)
-                        <= (firstVisibleItemPosition + visibleItemThreshold)) {
-                    // Estamos en el final, así que cargamos más contenido de la siguiente página
-                    loadingContent = true
-                    threadActualPage++
-
-                    // Obtenemos el número total de páginas que tiene el hilo. Puede haber cambiado
-                    // según hacíamos scroll, por lo que tenemos que comprobarlo cada vez que
-                    // se recarga
-                    FCThreadPagesObservable.create(threadLink).subscribe(
-                            {
-                                pages ->
-
-                                if (threadActualPage <= pages) {
-                                    // Cargamos más posts si hay más páginas
-                                    srlPostList.isRefreshing = true
-
-                                    loadThread(threadLink, threadActualPage, { posts -> postAdapter.addPosts(posts) })
-                                }
-                            },
-                            {
-                                error ->
-
-                                // Silenciamos los errores :)
-                            }
-                    )
-                }
-            }
-        })
+        )
     }
 }
