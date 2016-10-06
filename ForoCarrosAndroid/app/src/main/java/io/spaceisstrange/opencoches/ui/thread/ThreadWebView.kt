9eb46e0f193559dbe1930a590ad5e2243a4bf758
Fran González <fgonzaleva@gmail.com>
2016-10-06 15:40:23 +0200

    WebViews independientes para futuros usos

--- ForoCarrosAndroid/app/src/main/java/io/spaceisstrange/opencoches/ui/thread/ThreadWebView.kt
+++ ForoCarrosAndroid/app/src/main/java/io/spaceisstrange/opencoches/ui/thread/ThreadWebView.kt
@@ -16,34 +16,34 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package io.spaceisstrange.opencoches.ui.views
+package io.spaceisstrange.opencoches.ui.thread
 
 import android.content.Context
-import android.graphics.Color
 import android.util.AttributeSet
-import android.webkit.WebView
+import android.webkit.JavascriptInterface
+import io.spaceisstrange.opencoches.data.api.ApiConstants
+import io.spaceisstrange.opencoches.data.model.Post
+import io.spaceisstrange.opencoches.ui.templates.PostsTemplate
+import io.spaceisstrange.opencoches.ui.views.OpenCochesWebView
 
-abstract class OpenCochesWebView<in T> : WebView {
+class ThreadWebView : OpenCochesWebView<List<Post>> {
 
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    /**
-     * Método a llamar cuando el usuario presione en un usuario
-     */
-    var onUserClick: ((username: String, id: String, pictureSrc: String) -> Unit)? = null
-
     init {
-        // Hacemos el fondo transparente para que sea del color de la App
-        setBackgroundColor(Color.TRANSPARENT)
-
-        // Habilitamos JavaScript y nos declaramos como interfaz
-        settings.javaScriptEnabled = true
+        // Nos registramos como interfaz de JavaScript
+        addJavascriptInterface(this, "Android")
     }
 
-    /**
-     * Carga el contenido HTML especificado en el webview
-     */
-    abstract fun loadContent(content: T)
+    override fun loadContent(content: List<Post>) {
+        val contentHtml = PostsTemplate(context).render(content)
+        loadDataWithBaseURL(ApiConstants.BASE_URL, contentHtml, "text/html", "utf-8", null)
+    }
+
+    @JavascriptInterface
+    fun showProfile(username: String, id: String, pictureSrc: String) {
+        onUserClick?.invoke(username, id, pictureSrc)
+    }
 }
