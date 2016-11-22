a71d5d90ccdda04092fa2145544ffffae22a2ce0
Fran González <fgonzaleva@gmail.com>
2016-11-22 22:50:55 +0100

    Pequeño fix para versiones anteriores a la API 23

--- ForoCarrosAndroid/app/src/main/java/io/spaceisstrange/opencoches/ui/thread/ThreadFragment.kt
+++ ForoCarrosAndroid/app/src/main/java/io/spaceisstrange/opencoches/ui/thread/ThreadFragment.kt
@@ -23,6 +23,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import com.tinsuke.icekick.freezeInstanceState
 import com.tinsuke.icekick.state
 import com.tinsuke.icekick.unfreezeInstanceState
@@ -97,7 +98,7 @@ class ThreadFragment : Fragment(), ThreadContract.View {
         }
 
         // Ocultamos el fab de respuesta de respuesta al hacer scroll
-        wvPostContent.setOnScrollChangeListener {
+        (wvPostContent as WebView).setOnScrollChangeListener {
             view, x, y, oldX, oldY ->
 
             if (oldY - y <= 0) {
